e2402b7c46a9baa59e4cf1027d6d9697d7e2c334
Jeffrey Olchovy <olchovy@gmail.com>
2018-12-30 21:27:24 -0500

    naive integration tests for services

---
+++ common/src/it/scala/com/olchovy/jamie/EntityDetectionServiceSpec.scala
@@ -0,0 +1,31 @@
+package com.olchovy.jamie
+
+import scala.concurrent.ExecutionContext
+import org.scalatest.{AsyncFlatSpec, BeforeAndAfterAll, Matchers}
+
+class EntityDetectionServiceSpec extends AsyncFlatSpec with Matchers with BeforeAndAfterAll {
+
+  implicit val ece = ExecutionContext.global
+
+  override def beforeAll = {
+    require(
+      sys.env.contains("GOOGLE_APPLICATION_CREDENTIALS"),
+      """
+      |GOOGLE_APPLICATION_CREDENTIALS must be set and configured to target a 
+      |Google Cloud Platform project with the Natural Language API enabled.
+      """.stripMargin.trim
+    )
+  }
+
+  behavior of "EntityDetectionService"
+
+  it should "detect well-known entities in text input" in {
+    val input = "The president of the United States of America lives in the White House."
+    val expectedEntities = Set("United States of America", "White House")
+    for {
+      result <- EntityDetectionService(input)
+    } yield {
+      result.keySet should contain theSameElementsAs expectedEntities
+    }
+  }
+}
---
+++ common/src/it/scala/com/olchovy/jamie/SpeechToTextServiceSpec.scala
@@ -0,0 +1,31 @@
+package com.olchovy.jamie
+
+import scala.concurrent.ExecutionContext
+import org.scalatest.{AsyncFlatSpec, BeforeAndAfterAll, Matchers}
+
+class SpeechToTextServiceSpec extends AsyncFlatSpec with Matchers with BeforeAndAfterAll {
+
+  implicit val ece = ExecutionContext.global
+
+  override def beforeAll = {
+    require(
+      sys.env.contains("GOOGLE_APPLICATION_CREDENTIALS"),
+      """
+      |GOOGLE_APPLICATION_CREDENTIALS must be set and configured to target a 
+      |Google Cloud Platform project with the Speech-to-Text API enabled.
+      """.stripMargin.trim
+    )
+  }
+
+  behavior of "SpeechToTextService"
+
+  it should "offer transcriptions of audio streams" in {
+    val stream = getClass.getClassLoader.getResourceAsStream("audio/p1219-clip-1-mono.flac")
+    for {
+      results <- SpeechToTextService.stream(stream)
+      result = results.mkString(" ")
+    } yield {
+      result should (include("rabbit hole") and include("garage") and include("the show"))
+    }
+  }
+}
